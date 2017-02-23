45ea97bbe811db2838ae83832b10583aa8fa192a
yordank <yordankp@gmail.com>
2017-02-23 20:00:59 +0200

    Defining Classes 1 part

---
+++ C# OOP Basics/C# OOP Basics - Exercises/01. Define_a_Class_Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Define_a_Class_Person
+{
+    class Person
+    {
+
+        public string name;
+        public int age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            //Person person1 = new Person()
+            // {
+            //    name ="pesho",
+            //    age =31
+            // };
+
+            //Person person2 = new Person();
+
+            //person2.name = "gosho";
+            //person2.age = 30;
+
+            //var listPersons = new List<Person>();
+            //listPersons.Add(person1);
+            //listPersons.Add(person2);
+
+            //listPersons.OrderBy(x=>x.name).ToList().ForEach(x => Console.WriteLine(x.name + " " + x.age));
+
+
+        }
+
+    }
+}
---
+++ C# OOP Basics/C# OOP Basics - Exercises/03. Opinion Poll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Opinion_Poll
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var People = new List<Person>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var items = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(items[0], int.Parse(items[1]));
+                People.Add(person);
+
+            }
+
+            People.Where(x => x.age > 30).OrderBy(x => x.name).ToList().ForEach(x => Console.WriteLine(x.name + " - " + x.age));
+
+        }
+    }
+}
