ff5d2d997f29691ffcf43271a13a4e46c976e59f
yordank <yordankp@gmail.com>
2017-05-22 22:35:45 +0300

    implementation of binary heap

--- Data Structures-february2016/8. Binary-Heap/BinaryHeap/BinaryHeap.cs
+++ Data Structures-february2016/8. Binary-Heap/BinaryHeap/BinaryHeap.cs
@@ -1,50 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class BinaryHeap<T> where T : IComparable<T>
 {
+    public List<T> heap { get; set; }
     public BinaryHeap()
     {
-        // TODO
+        this.heap = new List<T>();
     }
 
     public BinaryHeap(T[] elements)
     {
-        // TODO
+        this.heap = new List<T>(elements);
+
+        for (int i = this.heap.Count/2; i >=0; i--)
+        {
+            HeapifyDown(i);
+        }
     }
 
     public int Count
     {
         get
         {
-            // TODO
-            throw new NotImplementedException();
+            return this.heap.Count;
         }
     }
 
     public T ExtractMax()
     {
-        // TODO
-        throw new NotImplementedException();
+        var max = this.heap[0];
+        this.heap[0] = this.heap[heap.Count - 1];
+        this.heap.RemoveAt(this.heap.Count - 1);
+
+        if(this.heap.Count>0)
+        {
+            HeapifyDown(0);
+        }
+
+        return max;
+
     }
 
     public T PeekMax()
     {
-        // TODO
-        throw new NotImplementedException();
+        var max = this.heap[0];
+
+        return max;
+
     }
 
     public void Insert(T node)
     {
-        // TODO
+        this.heap.Add(node);
+
+        HeapifyUp(this.heap.Count - 1);
+
     }
 
     private void HeapifyDown(int i)
     {
-        // TODO
+        var left = 2 * i + 1;
+        var right = 2 * i + 2;
+        var largest = i;
+
+        if(left<this.heap.Count&& this.heap[left].CompareTo(this.heap[largest])>0)
+        {
+            largest = left;
+        }
+
+        if(right<this.heap.Count&& this.heap[right].CompareTo(this.heap[largest])>0)
+        {
+            largest = right;
+        }
+
+        if(largest!=i)
+        {
+            T old = this.heap[i];
+            this.heap[i] = this.heap[largest];
+            this.heap[largest] = old;
+            HeapifyDown(largest);
+
+        }
+
+
     }
 
     private void HeapifyUp(int i)
     {
-        // TODO
+        var parent = (i - 1) / 2;
+        while(i>0 && this.heap[i].CompareTo(this.heap[parent])>0)
+        {
+            T old = this.heap[i];
+            this.heap[i] = this.heap[parent];
+            this.heap[parent] = old;
+
+
+            i = parent;
+            parent = (i - 1) / 2;
+
+        }
+
     }
 }
