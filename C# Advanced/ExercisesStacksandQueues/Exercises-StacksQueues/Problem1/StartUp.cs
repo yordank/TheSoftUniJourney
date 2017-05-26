3acaa80cc0602b6e571a25ead93b9661bf06eb66
yordank <yordankp@gmail.com>
2017-05-26 16:37:10 +0300

    problem1

---
+++ C# Advanced/ExercisesStacksandQueues/Exercises-StacksQueues/Problem1/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(new char[] { ' '},StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Stack<int> stack = new Stack<int>(nums);
+
+            while(stack.Count()>0)
+            {
+                Console.Write($"{stack.Pop()} ");
+            }
+
+        }
+    }
+}
