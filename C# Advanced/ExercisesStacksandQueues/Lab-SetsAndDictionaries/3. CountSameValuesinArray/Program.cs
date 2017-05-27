9f2f17bf28689e5c464cdebccd54848c746fde1f
yordank <yordankp@gmail.com>
2017-05-27 03:08:42 +0300

    Lab sets and Dictionaries

---
+++ C# Advanced/ExercisesStacksandQueues/Lab-SetsAndDictionaries/3. CountSameValuesinArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.CountSameValuesinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            SortedDictionary<int, int> dict = new SortedDictionary<int, int>();
+
+            foreach (var num in input)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 1);
+                }
+                else
+                {
+                    dict[num]++;
+                }
+
+
+            }
+
+            foreach (var dictElem in dict)
+            {
+                Console.WriteLine($"{dictElem.Key} - {dictElem.Value} times");
+            }
+
+        }
+
+    }
+}
---
+++ C# Advanced/ExercisesStacksandQueues/Lab-SetsAndDictionaries/4. Academy Graduation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Academy_Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedDictionary<string, List<double>> dict = new SortedDictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                var Name = Console.ReadLine();
+
+                var marks = Console.ReadLine().Trim().Split(new char[] {' ' },StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+
+                dict.Add(Name, marks);
+
+
+            }
+
+
+            foreach (var dictElem in dict.OrderBy(x=>x.Key).ThenBy(x=>x.Value.Average()))
+            {
+                Console.WriteLine($"{dictElem.Key} is graduated with {dictElem.Value.Average()}");
+            }
+
+
+        }
+    }
+}
