c590251b98dba4260c4f16daf6883d4391535b30
yordank <yordankp@gmail.com>
2017-05-30 02:04:55 +0300

    Lab: Trees and Traversals

---
+++ Data Structures-2017/03. Data-Structures-Basic-Trees-Lab-Skeleton/Trees/Trees/BinaryTree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+ 
+public class BinaryTree<T>
+{
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+
+    public BinaryTree<T> RighChild { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RighChild = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
+
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent + 1);
+        }
+
+        if (this.RighChild != null)
+        {
+            this.RighChild.PrintIndentedPreOrder(indent + 1);
+        }
+
+
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+
+        action(this.Value);
+
+        if (this.RighChild != null)
+        {
+            this.RighChild.EachInOrder(action);
+        }
+
+
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+
+        if (this.RighChild != null)
+        {
+            this.RighChild.EachPostOrder(action);
+        }
+
+        action(this.Value);
+
+    }
+}
+ 
