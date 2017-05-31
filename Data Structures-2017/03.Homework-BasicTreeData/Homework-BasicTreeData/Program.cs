b4dc1706eb04492ad51b3d50da1646ce4e8755c0
yordank <yordankp@gmail.com>
2017-05-31 03:23:33 +0300

    Basic Tree Data Structures - C# Exercise-all problems solved

--- Data Structures-2017/03.Homework-BasicTreeData/Homework-BasicTreeData/Program.cs
+++ Data Structures-2017/03.Homework-BasicTreeData/Homework-BasicTreeData/Program.cs
@@ -10,6 +10,11 @@ namespace Homework_BasicTreeData
     {
         static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
 
+        private static int deepestNodeLevel = int.MinValue;
+
+        private static int deepestNodeValue = int.MinValue;
+
+        private static List<Tree<int>> longestPathNodes = new List<Tree<int>>();
         static void Main()
         {
             ReadTree();
@@ -17,7 +22,170 @@ namespace Homework_BasicTreeData
             //Problem1();
             //Problem2();
             //Problem3();
-            Problem4();
+            //Problem4();
+            //Problem5();
+            //Problem6();
+            //Problem7();
+            Problem8();
+
+        }
+
+        private static void Problem8()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            var root = GetRootNode();
+
+            Console.WriteLine($"Subtrees of sum {num}:");
+
+            AllSubtreesWithGivenSum(root,num);
+
+        }
+
+        private static int AllSubtreesWithGivenSum(Tree<int>currentNode , int num)
+        {
+            int value = 0;
+
+            if (currentNode.Children.Count() == 0)
+            {
+                value = currentNode.Value;
+                if (value == num)
+                {
+                    Console.WriteLine(currentNode.Value);
+                }
+
+                return currentNode.Value;
+                                
+
+            }
+
+            
+
+            foreach (var child in currentNode.Children)
+            {
+                value += AllSubtreesWithGivenSum(child, num);
+            }
+
+            value += currentNode.Value;
+
+            if (value == num)
+            {
+                PrintSubTree(currentNode);
+                Console.WriteLine();
+            }
+             
+            return value;
+        }
+
+        private static void PrintSubTree(Tree<int> currentNode)
+        {
+            Console.Write($"{currentNode.Value} ");
+
+            foreach (var child in currentNode.Children)
+            {
+                PrintSubTree(child);
+            }
+
+
+        }
+
+
+        private static void Problem7()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            var root = GetRootNode();
+
+            Console.WriteLine($"Paths of sum {num}:");
+
+            AllPathsWithGivenSum(root, 0,num);
+
+
+        }
+
+        private static void AllPathsWithGivenSum(Tree<int> currentNode, int currentValue, int num)
+        {
+            int value = currentNode.Value + currentValue;
+
+            if (value == num)
+            {
+                longestPathNodes.Clear();
+
+                FindPath(currentNode);
+
+                longestPathNodes.Reverse();
+
+                Console.WriteLine($"{string.Join(" ", longestPathNodes.Select(x => x.Value))}");
+
+            }
+
+            else
+            {
+                foreach (var child in currentNode.Children)
+                {
+                    AllPathsWithGivenSum(child, value, num);
+                }
+
+            }
+               
+
+        }
+
+        private static void Problem6()
+        {
+            var root = GetRootNode();
+
+            DFS(root, 0);
+
+            longestPathNodes.Reverse();
+
+            Console.WriteLine($"Longest path: {string.Join(" ",longestPathNodes.Select(x=>x.Value))}");
+
+        }
+
+        private static void Problem5()
+        {
+            var root = GetRootNode();
+
+            DFS(root, 0);
+
+            Console.WriteLine($"Deepest node: {deepestNodeValue}");
+
+        }
+
+        private static void FindPath(Tree<int>currentNode)
+        {
+            longestPathNodes.Add(currentNode);
+
+
+            if (currentNode.Parent == null)
+                return;
+
+            FindPath(currentNode.Parent);
+
+
+
+        }
+        private static void DFS(Tree<int>currentNode,int level)
+        {
+            if (level > deepestNodeLevel)
+            {
+                deepestNodeLevel = level;
+
+                deepestNodeValue = currentNode.Value;
+
+                longestPathNodes.Clear();
+
+                FindPath(currentNode);
+
+            }
+
+            foreach (var child in currentNode.Children)
+            {
+                DFS(child, level + 1);
+            }
+
+
         }
 
         private static void Problem4()
