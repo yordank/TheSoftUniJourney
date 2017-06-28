e0d82c2ef85dd7d20c13642ac4aaf19a6d97e599
yordank <yordankp@gmail.com>
2017-06-29 02:42:54 +0300

    LimitedMemory only 22/27 tests passed //to do -almost completely new version :confounded:

---
+++ Data Structures-2017/LimitedMemory/LimitedMemory/C#-Skeleton/LimitedMemory/LimitedMemoryCollection.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using Wintellect.PowerCollections;
+using System;
+
+namespace LimitedMemory
+{
+    public class LimitedMemoryCollection<K, V> : ILimitedMemoryCollection<K, V>
+         where K : IComparable<K>
+    {
+
+        public static int number = 0;
+
+        private OrderedSet<Pair<K, V>> set = new OrderedSet<Pair<K, V>>();
+
+        private Dictionary<K, V> dict = new Dictionary<K, V>();
+
+        public LimitedMemoryCollection(int capacity)
+        {
+            this.capacity = capacity;
+    
+       
+            
+        }
+
+        public int capacity { get; set; }
+
+        public IEnumerator<Pair<K, V>> GetEnumerator()
+        {
+            foreach (var item in set)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            private set { }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return dict.Count;
+                
+            }
+            private set { }
+        }
+
+        public void Set(K key, V value)
+        {
+            if (capacity > dict.Count)
+            {
+                if (!dict.ContainsKey(key))
+                {
+                    dict.Add(key, value);
+                }
+                else
+                {
+                    dict[key] = value;
+
+                    Pair<K, V> item = set.Where(x => x.Key.CompareTo(key) == 0).First();
+
+                    set.Remove(item);
+
+                }
+
+                set.Add(new Pair<K, V>(key, value,number));
+
+                number++;
+
+                return;
+
+            }
+
+            if(dict.ContainsKey(key))
+            {
+
+                Pair<K, V> item = set.Where(x => x.Key.CompareTo(key) == 0).First();
+
+                set.Remove(item);
+
+                dict[key] = value;
+
+                set.Add(new Pair<K, V>(key, value, number++));
+                return;
+            }
+
+            else
+            {
+               
+                                
+                    var pair = set.RemoveLast();
+
+                    dict.Remove(pair.Key);
+
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict.Add(key, value);
+                    }
+                    else
+                    {
+                        dict[key] = value;
+                    }
+                    set.Add(new Pair<K, V>(key, value, number++));
+                     
+                
+            }
+
+        }
+
+        public V Get(K key)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();             
+            }
+            else
+            {
+                Pair<K,V> item=set.Where(x=>x.Key.CompareTo(key)==0).First();
+                set.Remove(item);
+
+                set.Add(new Pair<K, V>(key, dict[key],number++));
+
+                return dict[key];
+            }
+               
+        }
+    }
+}
---
+++ Data Structures-2017/LimitedMemory/LimitedMemory/C#-Skeleton/LimitedMemory/Pair.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LimitedMemory
+{
+    public class Pair<K, V>:IComparable<Pair<K,V>>
+        where K : IComparable<K>
+    {
+        public Pair(K key,V value,int hit)
+        {
+            this.Key=key;
+            this.Value=value;
+            this.Hit = hit;
+        }
+
+
+        public K Key { get; set; }
+
+        public V Value { get; set; }
+
+        public int Hit { get; set; }
+        public int CompareTo(Pair<K, V> other)
+        {
+            return -this.Hit.CompareTo(other.Hit);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Pair<K,V>;
+
+            return this.Key.CompareTo(item.Key)==0;
+        }
+
+         
+
+    }
+}
