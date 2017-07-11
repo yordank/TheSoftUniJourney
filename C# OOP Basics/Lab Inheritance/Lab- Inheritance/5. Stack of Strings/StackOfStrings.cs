07745dd8cf1f4a3c72a8c5ada148817c1ede9857
yordank <yordankp@gmail.com>
2017-07-11 21:13:31 +0300

    lab inheritance

---
+++ C# OOP Basics/Lab Inheritance/Lab- Inheritance/5. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+    public class StackOfStrings
+    {
+        public StackOfStrings()
+        {
+            data = new List<string>();
+        }
+         
+        private List<string> data;
+        public void Push(string item)
+        {
+            data.Insert(0, item);
+        }
+
+        public string Pop()
+        {
+            var result = data[0];
+            data.RemoveAt(0);
+            return result;
+        }
+        public string Peek()
+        {
+            return data[0];
+        }
+
+        public bool IsEmpty()
+        {
+            return data.Count() == 0;
+        }
+    }
+ 
