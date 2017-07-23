1a1d7d383971ed31046468114a1a69026eb936a4
yordank <yordankp@gmail.com>
2017-07-24 00:05:37 +0300

    Lab-Interfaces and Abstraction

---
+++ C#OOP Advanced/Lab-Interfaces and Abstraction/2. Cars/Seat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        this.Color = color;
+        this.Model = model;
+    }
+    public string Color
+    {
+        get;
+        private set;
+    }
+
+    public string Model
+    {
+        get;
+        private set;
+    }
+
+    public string Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Stop()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
---
+++ C#OOP Advanced/Lab-Interfaces and Abstraction/2. Cars/Tesla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public int Batteries { get; private set; }
+    public Tesla(string model, string color, int batteries)
+    {
+        this.Model = model;
+
+    }
+    public string Start()
+    {
+        return "Car started!";
+    }
+    public string Stop()
+    {
+        return "Car stopped!";
+    }
+}
+
+
