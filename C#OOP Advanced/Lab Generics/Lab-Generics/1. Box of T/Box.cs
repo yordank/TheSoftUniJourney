acec63467de9e796577425413515c13de8d9cc23
yordank <yordankp@gmail.com>
2017-07-26 12:32:55 +0300

    lab generics

---
+++ C#OOP Advanced/Lab Generics/Lab-Generics/1. Box of T/Box.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    private List<T> data;
+
+    
+
+    public int Count => this.data.Count;
+    public void Add(T item)
+    { this.data.Add(item); }
+    public T Remove()
+    {
+        var rem = this.data.Last();
+        this.data.RemoveAt(this.data.Count - 1);
+        return rem;
+    }
+}
---
+++ C#OOP Advanced/Lab Generics/Lab-Generics/2. Generic Array Creator/ArrayCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ArrayCreator
+{
+    public static T[] Create<T>(int lenght, T item)
+    {
+        T[] array = new T[lenght];
+        return array;
+    }
+}
+
